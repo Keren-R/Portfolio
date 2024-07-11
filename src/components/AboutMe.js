1fa8e1a1fdc2c05a412bf5cd6de39030021a28ae
Keren Reshef <kerenreshef@Kerens-MacBook-Pro.local>
2024-07-11 13:44:30 +0300

    added photo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ export function AboutMe(){
         <div id="about-me" className="container col-xxl-8 px-4 py-5">
             <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div className="col-10 col-sm-8 col-lg-6">
-                    <img src={require("../images/pikachu.jpg")} className="d-block mx-lg-auto img-fluid profile-pic" alt="Keren Reshef"
+                    <img src={require("../images/keren.jpeg")} className="d-block mx-lg-auto img-fluid profile-pic" alt="Keren Reshef"
                          loading="lazy"/>
                 </div>
                 <div className="col-lg-6">
