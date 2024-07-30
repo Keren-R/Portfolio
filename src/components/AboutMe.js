74ae80793ef2ff113b74c3be3872c5f5a96b9f7f
Keren Reshef <kerenreshef@Kerens-MacBook-Pro-2.local>
2024-07-30 16:22:42 +0300

    edited text in AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,9 +13,9 @@ export function AboutMe(){
                 </div>
                 <div className="col-lg-6">
                     <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Hi, i'm Keren Reshef</h1>
-                    <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the
-                        world’s most popular front-end open source toolkit, featuring Sass variables and mixins,
-                        responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <p className="lead">I am a FullStack developer proficient in Python, React, JavaScript, HTML and CSS. <br />
+                        Experienced in Django, Pytest, AWS, Docker, CI/CD, GitHub and Agile/Scrum development. <br />
+                        I am a problem-solver and quick learner, capable of understanding and working with complex systems and architectures from the top abstract level down to the actual implementation.</p>
                     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
                         <button type="button" className="btn btn-outline-secondary btn-lg px-4" onClick={handleResumeClick}>Resume</button>
                     </div>
